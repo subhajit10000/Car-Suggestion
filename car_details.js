bec3859ea47b156a51fb37df5ef5efe713fba559
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-04-28 21:09:48 +0530

    Add files via upload

---
+++ car_details.js
@@ -0,0 +1,118 @@
+export const car = {
+    "Diesel":[
+    {
+      model: "Mahindra Scorpio N",
+      price: "₹ 12.5 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/mahindra/scorpio/mahindra-scorpio-6-1766743994.png?w=850&q=40",
+    },
+    {
+      model: "Toyota Innova Crysta",
+      price: "₹ 21.7 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/toyota/innova-crysta/toyota-innova-crysta-3-1767849001.png?w=1100&q=40",
+    },
+    {
+      model: "Maruti Suzuki Ertiga BS6",
+      price: "₹ 11.9 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/maruti/ertiga/maruti-ertiga-4-1767874534.png",
+    },
+    {
+      model: "Mahindra Bolero",
+      price: "₹ 10.59 Lakhs",
+      image: "https://banner2.cleanpng.com/20180410/ake/avgwa449x.webp",
+    },
+    {
+      model: "Toyota Fortuner",
+      price: "₹ 47.6 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/toyota/fortuner-legender/toyota-fortuner-legender-0-1767849483.png?w=850&q=40",
+    },
+    {  
+      model: "MG Gloster BS6",
+      price: "₹ 34.7 Lakhs",
+      image: "https://prod2.singleinterface.com//files/enterprise/additionalfeatureimage/additionalimages/349472/ADDITIONAL-FEATURE-PRODUCT-DETAILS23860-Dealer-Microsite-assets-Gloster-02-30-12-24-03-45-23.png",
+    }
+    ],
+    "Petrol":[
+    {
+      model: "Mahindra Scorpio N",
+      price: "₹ 12.5 Lakhs",
+      image: "https://i.pinimg.com/originals/f2/cf/5e/f2cf5ef4e4b51d29e3420fc32105c3ca.jpg?nii=t",
+    },
+    {
+      model: "Toyota Innova Hycross",
+      price: "₹ 21.7 Lakhs",
+      image: "https://akm-img-a-in.tosshub.com/aajtak/images/photo_gallery/202211/toyota888787.jpg",
+    },
+    {
+      model: "TATA Punch",
+      price: "₹ 5.65 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/tata/punch/tata-punch-0-1769487768.png?w=800&q=40",
+    },
+    {
+      model: "Toyota Innova Hycross",
+      price: "₹ 21.7 Lakhs",
+      image: "https://static.india.com/wp-content/uploads/2023/10/Best-Mileage-5-Petrol-Cars-Under-6-Lakh.png?impolicy=Medium_Widthonly&w=400&h=800",
+    },   
+    {
+      model: "Maruti Suzuki Ertiga BS6",
+      price: "₹ 11.9 Lakhs",
+      image: "https://i.pinimg.com/736x/3a/52/47/3a5247de5990c5a83459645625f8d6e8.jpg",
+    },
+    {
+      model: "Mahindra THAR",
+      price: "₹ 10.59 Lakhs",
+      image: "https://www.pngall.com/wp-content/uploads/19/Thar-Car-Robust-Build-Quality-PNG.png",
+    },
+    {
+      model: "Toyota Fortuner",
+      price: "₹ 47.6 Lakhs",
+      image: "https://toppng.com/uploads/preview/toyota-cars-png-file-11664272840aupqps2kgy.png",
+    },
+    {  
+      model: "Jeep Compass BS6",
+      price: "₹ 34.7 Lakhs",
+      image: "https://www.kindpng.com/picc/m/177-1777493_jeep-compass-white-2017-india-hd-png-download.png",
+    }
+    ],
+    "EV":[
+    {
+      model: "Mahindra XUV E8",
+      price: "₹ 12.5 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/mahindra/xev-9s/mahindra-xev-9s-6-1766745068.png?w=800&q=40",
+    },
+    {
+      model: "Mahindra BE 6",
+      price: "₹ 12.5 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/mahindra/be-6e/mahindra-be-6e-4-1767931326.png?w=850&q=40",
+    },
+    {
+      model: "VinFast VF 6",
+      price: "₹ 12.5 Lakhs",
+      image: "https://imgd.aeplcdn.com/664x374/n/cw/ec/171545/vf-6-exterior-front-view-45.jpeg?isig=0&q=80",
+    },    
+    {
+      model: "Toyota Z6 EV",
+      price: "₹ 21.7 Lakhs",
+      image: "https://png.pngtree.com/png-clipart/20250516/original/pngtree-large-electric-family-car-eco-friendly-6-seater-vehicle-png-image_21006196.png",
+    },
+    {
+      model: "Maruti Suzuki Vitara EV",
+      price: "₹ 11.9 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/maruti/e-vitara/maruti-e-vitara-0-1769588233.png?w=850&q=40",
+    },
+    {
+      model: "Mahindra XUV E5",
+      price: "₹ 10.59 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/mahindra/xuv-3xo-ev/mahindra-xuv-3xo-ev-0-1768390523.png?w=850&q=40",
+    },
+    {
+      model: "Toyota VZ4",
+      price: "₹ 47.6 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/toyota/urban-cruiser-ebella/toyota-urban-cruiser-ebella-0-1769588323.png?w=800&q=40",
+    },
+    {  
+      model: "MG ZS EV",
+      price: "₹ 34.7 Lakhs",
+      image: "https://images.91wheels.com/assets/c_images/gallery/mg/windsor-ev/mg-windsor-ev-4-1767846845.png?w=1100&q=40",
+    }     
+    ]
+}
