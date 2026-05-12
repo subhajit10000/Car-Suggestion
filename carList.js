992f6b596772b6a27f33e5446dbd6797ae0f9d5e
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-05-12 20:03:45 +0530

    Add files via upload

--- carList.js
+++ carList.js
@@ -1,58 +1,3 @@
-// import { car } from './car_details.js';
-
-// function populateProducts() {
-//     const productList = document.getElementById('productList');
-//     const queryParams = new URLSearchParams(window.location.search);
-//     const queryParamsObject = Object.fromEntries(queryParams.entries());
-
-//     const category = queryParamsObject['category'];
-    
-//     const categoryItems = car [category];
-
-    
-//     categoryItems.forEach((item) => {
-//         const productListBox = document.createElement('div');
-//         const productItem = document.createElement('a');
-//         productItem.className = 'product-item';
-//         const productImage = document.createElement('div');
-//         const cardBody = document.createElement('div')
-//         const productModel = document.createElement('div');
-//         const productPrice = document.createElement('div');
-
-//         const imageData = document.createElement('img');
-//         imageData.src = item.image;
-//         productListBox.className = 'product-list-box';
-//         cardBody.className = 'card-body';
-//         productImage.className = 'product-img';
-//         productModel.className = ('product-model');
-//         productPrice.className = ('product-price');
-
-//         productImage.appendChild(imageData);
-//         productModel.textContent = item.model;
-//         productPrice.textContent = item.price;
-
-//         productItem.appendChild(productImage);
-//         cardBody.appendChild(productModel);
-//         cardBody.appendChild(productPrice);
-
-//         productItem.appendChild(productImage);
-//         productItem.appendChild(cardBody)
-//         productListBox.appendChild(productItem)
-//     })
-// }
-
-// populateProducts();
-
-
-
-
-
-
-
-
-
-
-
 import { car } from './car_details.js';
 
 
@@ -60,13 +5,21 @@ function populateProducts() {
   const productList = document.getElementById('productList');
 
   const queryParams = new URLSearchParams(window.location.search);
-  const category = queryParams.get('category'); 
+  const queryParamsObject = Object.fromEntries(queryParams.entries());  
+  const category = queryParamsObject['category'];
 
-  const categoryItems = car[category];
+    let categoryItems = [];
 
+    if(category) {
+        categoryItems = car[category];
+    } else {
+        categoryItems = Object.values(car).flat();
+    }
+    
 
   categoryItems.forEach((item) => {
     const productItem = document.createElement('a');
+    productItem.href = `product.html?product-id=${item.id}`;
     productItem.className = 'product-item';
 
     const productImage = document.createElement('div');
@@ -79,6 +32,10 @@ function populateProducts() {
 
     const cardBody = document.createElement('div');
     cardBody.className = 'card-body';
+
+    const productName = document.createElement('div');
+    productName.className=('product-name');
+    productName.textContent = item.name;
 
     const productModel = document.createElement('div');
     productModel.className = 'product-model';
@@ -93,6 +50,8 @@ function populateProducts() {
 
     productItem.appendChild(productImage);
     productItem.appendChild(cardBody);
+    productItem.appendChild(productName);
+
 
     productList.appendChild(productItem); 
   })
