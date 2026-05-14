2117fe247f545afdcbb441592128d02c87eab721
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-05-14 09:22:15 +0530

    Update cartStore.js

--- cartStore.js
+++ cartStore.js
@@ -1,5 +1,4 @@
 export const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-
 export function saveCartItems() {
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
 }
